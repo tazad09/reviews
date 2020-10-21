7811d37ea3628174b17f247597b95410633b6ea2
Tasnim <tasnimazad17@gmail.com>
2020-10-21 11:35:12 -0400

    made some changes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const router = require("./routers.js")
 
 mongoose.connect('mongodb://mongo:27017/reviewsdb', () => console.log('connected!'))
+//host.docker.internal gets the data from the local database
 //mongoose.connect('mongodb://host.docker.internal:27017/reviewsdb', () => console.log('connected!'))
 
 
